81ca66cbb54fd865b3d6030270d8a6c14447a67b
Sagar Yadav <sagaryadav@Sagars-MacBook-Pro.local>
2018-12-18 17:31:20 +0530

    init commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
+v
+Fapp/src/main/java/com/example/sagaryadav/yodaandroid/MainActivity.java,d/5/d53f046225d6e5b97e2d1bab6548de8daac3e9fe
---
+++ app/src/main/java/com/example/sagaryadav/yodaandroid/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.sagaryadav.yodaandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    Button btnClickHere;
+    Button btnStart;
+    TextView tvMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: ");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //bind views
+        btnClickHere = findViewById(R.id.btnClickHere);
+        btnStart = findViewById(R.id.btnStart);
+        tvMessage = findViewById(R.id.tvMessage);
+
+        btnClickHere.setOnClickListener((v) -> {
+            showAlertMessage(R.string.app_name);
+        });
+
+        btnStart.setOnClickListener((v) -> {
+
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart: ");
+        super.onStart();
+    }
+
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume: ");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause: ");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop: ");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: ");
+        super.onDestroy();
+    }
+
+    public void showAlertMessage(int stringResId) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Alert");
+        builder.setMessage("Alert Message");
+        builder.create().show();
+    }
+
+}
